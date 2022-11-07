8db3b6b9c57a687fec0fec3a24627d55b9e65ce4
AnilDaal <anildaal7@gmail.com>
2022-11-07 18:11:08 +0530

    This is subset of a string

---
+++ Rec08.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+public class Rec08{
+    static String swap(int position1,int position2,String s){
+        char[] charArr = s.toCharArray();
+        char c1=s.charAt(position1),c2=s.charAt(position2);
+        charArr[position2]=c1;charArr[position1]=c2;
+        s = charArr.toString();
+        return s;
+    }
+    public static void fun(int n,String str)
+    {
+        if(n==(str.length()-1)){
+            System.out.println(str);
+            return;
+        }
+        for(int i=n;i<str.length();i++){
+        tringBuilder s = new StringBuilder(str);
+        s.setCharAt(n,str.charAt(i));
+        s.setCharAt(i,str.charAt(n));
+        str = s.toString();
+        
+            fun(n+1,str);
+        s.setCharAt(n,str.charAt(i));
+        s.setCharAt(i,str.charAt(n));
+        str = s.toString();
+            }
+    }
+    public static void main(String args[]){
+        fun(0,"ABCD");
+    }
+}
